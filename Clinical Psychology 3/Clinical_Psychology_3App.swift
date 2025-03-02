fba3a04833ce0c120451399242ceaa8031aa1413
muzafferse <muzaffer.sevili@vestel.com.tr>
2025-03-02 22:44:29 +0300

    Initial Commit

---
+++ Clinical Psychology 3/Clinical_Psychology_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Clinical_Psychology_3App.swift
+//  Clinical Psychology 3
+//
+//  Created by Muzaffer Sevili on 2.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Clinical_Psychology_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
