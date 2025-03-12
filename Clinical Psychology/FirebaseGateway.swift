85ee0e04ec47c15dba0fd80ad4079a36ffd8dd9c
muzafferse <muzaffer.sevili@vestel.com.tr>
2025-03-12 07:47:49 +0300

    fix googleService info file problem.

--- Clinical Psychology/FirebaseGateway.swift
+++ Clinical Psychology/FirebaseGateway.swift
@@ -10,6 +10,15 @@ import Firebase
 public final class FirebaseGateway {
     /// Configures the Firebase SDK.
     public static func configure() {
-        FirebaseApp.configure()
+        #if CLINIC3A
+        let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+        #elseif CLINIC3B
+        let filePath = Bundle.main.path(forResource: "GoogleService-Info_3B", ofType: "plist")
+        #endif
+        if let filePath = filePath,
+        let options = FirebaseOptions(contentsOfFile: filePath) {
+            FirebaseApp.configure(options: options)
+        }
+        
     }
 }
